07e08866d8089346673e994c2cceccc5e6154445
Oleksandr <71662702+alexpolishchuck@users.noreply.github.com>
2025-05-20 12:13:26 +0300

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,6 +1,10 @@
-set "BOOST_INCLUDE=D:\sources\_uss-vc141\boost_1_85_0"
 echo off
-call "%USS_VS_ROOT_2022%\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+set "BOOST_INCLUDE=D:\sources\boost_1_85_0"
+set "VS_ROOT=C:\Program Files\Microsoft Visual Studio\2022\Professional"
+
+call "%VS_ROOT%\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 @echo on
 
 devenv .\TestTaskAtto.sln
